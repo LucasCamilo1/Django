3685c40a12f95adf89ab3237eacd5dd16012ce8f
LucasCamilo1 <lucascamilo326@gmail.com>
2023-05-03 01:45:35 -0300

    Processo em andamento

---
+++ filme/urls.py
@@ -0,0 +1,12 @@
+#url - view - template 
+
+from django.urls import path, include
+from .views import homepage, homefilmes
+
+
+
+urlpatterns = [
+    path('', homepage),
+    path('filmes/', homefilmes)
+    
+]
---
+++ filme/views.py
@@ -0,0 +1,16 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import Filme
+
+# Create your views here.
+
+def homepage(request):
+    return render(request,"homepage.html")
+
+#
+def homefilmes(request):
+    context = {}
+    lista_filmes = Filme.objects.all()
+    context ['lista_filmes'] = lista_filmes
+
+    return render(request, "homefilmes.html", context)
